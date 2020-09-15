c21d5e38dc6efbe146422decd6dda022d4ab4c0e
dhsathiya <devarshisathiya5@gmail.com>
2020-09-15 23:31:55 +0530

    Fix installation condition
    Signed-off-by: dhsathiya <devarshisathiya5@gmail.com>

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ umacro_version="v0.0.2"
 [[ -d /etc/umacro/ ]] || mkdir /etc/umacro
 [[ -f /etc/umacro/umacro_conf.yml ]] || touch /etc/umacro/umacro_conf.yml
 
-[[ -f /usr/local/bin/umacro ]] || rm /usr/local/bin/umacro
+[[ -f /usr/local/bin/umacro ]] && rm /usr/local/bin/umacro
 
 wget -P /usr/local/bin https://github.com/dhsathiya/UMacro/releases/download/$umacro_version/umacro -q --show-progress
 
