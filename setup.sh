c0a5db87c7f9f4609825e17013238b775952e8bf
dhsathiya <devarshisathiya5@gmail.com>
2020-09-15 23:18:48 +0530

    Fix installation step
    Signed-off-by: dhsathiya <devarshisathiya5@gmail.com>

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@ umacro_version="v0.0.2"
 [[ -d /etc/umacro/ ]] || mkdir /etc/umacro
 [[ -f /etc/umacro/umacro_conf.yml ]] || touch /etc/umacro/umacro_conf.yml
 
-rm /usr/local/bin/umacro
+[[ -f /usr/local/bin/umacro ]] || rm /usr/local/bin/umacro
+
 wget -P /usr/local/bin https://github.com/dhsathiya/UMacro/releases/download/$umacro_version/umacro -q --show-progress
 
 chmod +x /usr/local/bin/umacro
